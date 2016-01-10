88dbe94d2dbdc00c2d9cfc37d2c930441d196cd9
jackforney <jack.forney@yahoo.com>
2016-01-10 13:10:31 -0800

    Create id_dashbutton.py

---
+++ id_dashbutton.py
@@ -0,0 +1,16 @@
+#Similar to find_dashbutton.py
+#Prints defined name of dashbutton using hard-coded MAC address
+
+from scapy.all import *
+
+def arp_display(pkt):
+  if pkt[ARP].op == 1: #who-has (request)
+    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      if pkt[ARP].hwsrc == 'a0:02:dc:0c:43:d3':  #detect button-specific MAC Address
+        print 'Pressed ON Button'
+      #if pkt[ARP].hwsrc == '<another MAC Address>':
+        #print 'Press <another button>'
+      else:
+        print "ARP Probe from: " + pkt[ARP].hwsrc
+
+print sniff(prn=arp_display, filter="arp", store=0, count=10)
